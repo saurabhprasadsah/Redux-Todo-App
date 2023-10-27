4b820c4b2ed71c0ee5e870df36e61ef92bcbcd5d
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-27 20:14:34 +0530

    advnaceobjects

--- src/store/reducer/todo.reducer.js
+++ src/store/reducer/todo.reducer.js
@@ -5,8 +5,11 @@ const initialstate = {
 export const TodoReducer = (state = initialstate, action) => {
    console.log(action);
    switch (action.type) {
+      
       case "ADD_TODO" :
+
       let array  =[...state.todos, action.payload]
+
       return{
          ...state,
          todos:array
