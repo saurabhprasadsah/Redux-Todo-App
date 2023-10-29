1b3b881bfbb6cf568413aa35237f81a473ec70ed
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-29 21:48:49 +0530

    todoreduxapp

--- src/store/reducer/todo.reducer.js
+++ src/store/reducer/todo.reducer.js
@@ -2,15 +2,11 @@
 const initialstate = {
    todos: ['breakfast', 'Lunch', 'dinner']
 }
-
 export const TodoReducer = (state = initialstate, action) => {
    console.log(action);
    switch (action.type) {
-
       case "ADD_TODO" :
-
       let array  =[...state.todos, action.payload]
-
       return{
          ...state,
          todos:array
