f274ea135f3ed44095e94f4c036ad8d14860b3ab
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-31 20:51:57 +0530

    edittodoapp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/class/App';
+
+//Use for functional components...
 // import App from './components/functional/App';
 import { Provider } from 'react-redux';
 import store from './store/store/store';
