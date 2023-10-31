15442cd5d37c6efd2f7da2954fc9a11f6edd1142
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-31 16:48:06 +0530

    edittodoapp

--- src/components/class/App.jsx
+++ src/components/class/App.jsx
@@ -1,14 +1,14 @@
-// import React, { Component } from 'react'
-// import List from './List'
-// import Input from './input'
+import React, { Component } from 'react'
+import List from './List'
+import Input from './input'
 
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <List/>
-//         <Input/>
-//       </div>
-//     )
-//   }
-// }
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <List/>
+        <Input/>
+      </div>
+    )
+  }
+}
