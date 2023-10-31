93da4677f9d8a9a5bc3ab75dac264240012bea20
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-31 15:52:28 +0530

    todoreduxapp

--- src/components/functional/input.jsx
+++ src/components/functional/input.jsx
@@ -8,28 +8,26 @@ export default function Input() {
   let [error, setError] = useState(false);
 
 
-  const inputChange = (event) =>{
+  const inputChange = (event) => {
     setTodo(event.target.value)
   }
 
-  const submit = (event) =>{
+  const submit = (event) => {
 
     event.preventDefault();
 
-    if(todo.length > 0){
+    if(todo.length > 0) {
       dispatch({
-        type:"ADD_TODO",
+        type: "ADD_TODO",
         payload: todo
 
       })
       setTodo('');
 
-    } else{
-
+    } else {
+      setError(true);
 
     }
-
-
   }
 
 
@@ -37,20 +35,23 @@ export default function Input() {
     <form className="row" onSubmit={submit}>
       <div className="col-10">
         <input type="text"
-         className="form-control"
-         placeholder="Enter Todo"
-         value={todo}
-         onChange={inputChange}/>
+          className="form-control"
+          placeholder="Enter Todo"
+          value={todo}
+          onChange={inputChange} />
 
-           {
-              error &&  <p className='text-danger'> Please enter todo </p>
-           }
+        {
+          error && <p className='text-danger'> Please enter todo </p>
+        }
 
       </div>
 
       <div className="col-2">
 
-        <button type="submit" className="btn btn-primary mb-3">Add</button>
+        <button type="submit"
+          className="btn btn-primary mb-3"
+          disabled={todo.length === 0 ? true : false}>Add</button>
+
       </div>
     </form>
   )
